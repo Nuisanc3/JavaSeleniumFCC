2850847abc4eac3d88829befdadc725a47fb1ff4
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-02 22:04:52 +0530

    Fixed login page

--- SeleniumWebDriverFCC/src/main/java/com/saucedemo/pages/LoginPage.java
+++ SeleniumWebDriverFCC/src/main/java/com/saucedemo/pages/LoginPage.java
@@ -6,14 +6,23 @@ public class LoginPage extends BasePage {
 
     // All the methods will be inherited from BasePage
 
-    private By username = By.id("user-name");
-    private By password = By.id("password");
+    private By usernameFiled = By.id("user-name");
+    private By passwordField = By.id("password");
     private By loginButton = By.id("login-button");
     private By errorMessage = By.xpath("#login_button_container h3");
 
-    
 
 
+public void setUsername (String username) {
+    set(usernameFiled, username);
+}
 
+public void setPassword (String password) {
+    set(passwordField, password);
+}
+
+public void clickLoginButton() {
+    click(loginButton);
+}
 
 }
