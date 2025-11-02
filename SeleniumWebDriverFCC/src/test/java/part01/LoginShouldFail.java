17d2ef56094f6d66bb2cf8d3a55c2ff8a0715f24
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-02 21:29:54 +0530

    Test Failed login using TestNG

---
+++ SeleniumWebDriverFCC/src/test/java/part01/LoginShouldFail.java
@@ -0,0 +1,60 @@
+package part01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginShouldFail {
+
+    WebDriver driver ;
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    @Test
+    public void testLoggingIntoApplication() throws InterruptedException {
+        Thread.sleep(30000);
+        WebElement username = driver.findElement(By.name("username"));
+        username.click();
+        username.clear();
+        username.sendKeys("Admin");
+
+
+        var password = driver.findElement(By.name("password"));
+        password.click();
+        password.clear();
+        password.sendKeys("admin123");
+
+
+        driver.findElement(By.tagName("button")).click();
+        Thread.sleep(5000);
+        String actualResult = driver.findElement(By.tagName("h6")).getText();
+        String expectedResult = "Dashboard";
+
+        Assert.assertNotEquals(actualResult, expectedResult);
+
+
+
+
+    }
+
+}
+
+
+
