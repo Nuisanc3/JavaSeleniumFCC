82e71d7207958e8578013036426e4093c86a3fb8
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-05 08:51:36 +0530

    Created a simple test for bad response link

---
+++ SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part03/elements/LinksTest.java
@@ -0,0 +1,18 @@
+package part3_4.com.demoqa.test.part03.elements;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part3_4.com.demoqa.base.BaseTest;
+
+public class LinksTest extends BaseTest {
+
+    @Test
+    public void testLinks() {
+        var linksPage = homePage.gotoElements().clickLinks();
+        linksPage.clickBadRequestLink();
+        String actualResponse = linksPage.getResponse();
+        Assert.assertTrue(actualResponse.contains("400") && actualResponse.contains("Bad Request"), "\n Actual Response (" + actualResponse + ") \n Does not contain '400' and 'Bad Request' text.");
+    }
+
+
+}
