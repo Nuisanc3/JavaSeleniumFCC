133b09b44f79543e839fc4c4695013d613b7115f
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-07 21:06:34 +0530

    Added screenshot method in the base test

--- SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/base/BaseTest.java
+++ SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/base/BaseTest.java
@@ -2,11 +2,21 @@ package part3_4.com.demoqa.base;
 
 import com.demoqa.pages.HomePage;
 import com.saucedemo.pages.BasePage;
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITest;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
 
 import static utility.Utility.setUtilityDriver;
 
@@ -32,6 +42,26 @@ public class BaseTest {
         setUtilityDriver();
         homePage = new HomePage();
     }
+
+    @AfterMethod
+    public void takeFailedResultScreenshot(ITestResult testResult) {
+        if(ITestResult.FAILURE == testResult.getStatus()) {
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            File source = screenshot.getScreenshotAs(OutputType.FILE);
+            File destination = new File(System.getProperty("user.dir") +
+                    "/resources/screenshots/(" +
+                    java.time.LocalDate.now() +
+                    testResult.getName() + ".png");
+            try {
+                FileHandler.copy(source, destination);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Screenshot located at" + destination);
+        }
+    }
+
+
 
     @AfterClass
     public void tearDown() {
