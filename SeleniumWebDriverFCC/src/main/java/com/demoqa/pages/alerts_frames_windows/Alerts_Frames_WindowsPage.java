8e6acdf11e3c52e8010ec9a44ce78928d5584d5d
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-09 20:59:13 +0530

    CReated POM setup for the FramesTest

--- SeleniumWebDriverFCC/src/main/java/com/demoqa/pages/alerts_frames_windows/Alerts_Frames_WindowsPage.java
+++ SeleniumWebDriverFCC/src/main/java/com/demoqa/pages/alerts_frames_windows/Alerts_Frames_WindowsPage.java
@@ -9,6 +9,15 @@ public class Alerts_Frames_WindowsPage extends HomePage {
 
     private By modalDialogsMenuItem = By.xpath("//li[@id='item-4']/span[text()='Modal Dialogs']");
     private By alertsMenuItem = By.xpath("//li[@id='item-1']/span[text()='Alerts']");
+    private By framesMenuItem = By.xpath("//li[@id='item-2']/span[text()='Frames']");
+
+    public FramesPage clickFrames() {
+        scrollToElementJS(framesMenuItem);
+        click(framesMenuItem);
+        return new FramesPage();
+
+    }
+
 
     public AlertsPage clickAlerts() {
         scrollToElementJS(alertsMenuItem);
---
+++ SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/frames/FramesTest.java
@@ -0,0 +1,4 @@
+package part3_4.com.demoqa.test.part04.frames;
+
+public class FramesTest {
+}
