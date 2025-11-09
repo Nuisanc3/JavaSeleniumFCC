b39e1c1fd9009e7601cc01d33234bebb0a220486
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-09 21:17:06 +0530

    FramesTest

--- SeleniumWebDriverFCC/src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
+++ SeleniumWebDriverFCC/src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
@@ -1,4 +1,15 @@
 package com.demoqa.pages.alerts_frames_windows;
 
+import org.openqa.selenium.By;
+
 public class FramesPage extends Alerts_Frames_WindowsPage{
+
+    private By textInFrames = By.id("sampleHeading");
+
+
+    public String getTextInBigFrame() {
+        String bigFrameText = find(textInFrames).getText();
+        System.out.println(bigFrameText);
+        return bigFrameText;
+    }
 }
--- SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/frames/FramesTest.java
+++ SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/frames/FramesTest.java
@@ -1,4 +1,21 @@
 package part3_4.com.demoqa.test.part04.frames;
 
-public class FramesTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part3_4.com.demoqa.base.BaseTest;
+
+public class FramesTest extends BaseTest {
+
+
+    @Test
+    public void testFramesBigBox() {
+        var framesPage = homePage.goToAlertsFramesWindowsCard().clickFrames();
+        String actualBigBoxText = framesPage.getTextInBigFrame();
+        String expectedBigBoxText = "This is a sample page";
+        Assert.assertEquals(actualBigBoxText, expectedBigBoxText, "\n Actual and Expected text do not match \n");
+
+    }
+
+
+
 }
