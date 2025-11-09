f1b96cb1f73862cba2a9633b1bcade8678ac2f62
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-09 21:55:19 +0530

    Handled frames switch properly

--- SeleniumWebDriverFCC/src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
+++ SeleniumWebDriverFCC/src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
@@ -6,10 +6,24 @@ public class FramesPage extends Alerts_Frames_WindowsPage{
 
     private By textInFrames = By.id("sampleHeading");
 
+    // Notice we have user Sring instead of the By, we are going to switch to the frame using the string.
+    private String iFrameBigBox = "frame1";
+
+    private By headerFramesText = By.xpath("//div[@id='app']//h1[text()='Frames']");
+
+    private void switchToBigBox() {
+        driver.switchTo().frame(iFrameBigBox);
+    }
+
 
     public String getTextInBigFrame() {
+        switchToBigBox();
         String bigFrameText = find(textInFrames).getText();
         System.out.println(bigFrameText);
         return bigFrameText;
+    }
+
+    public String getHeaderFramesText() {
+        return find(headerFramesText).getText();
     }
 }
--- SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/frames/FramesTest.java
+++ SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/frames/FramesTest.java
@@ -16,6 +16,8 @@ public class FramesTest extends BaseTest {
 
     }
 
+    
+
 
 
 }
