a1753245d4d3d9a93dd860e672cb534b686d20e4
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-11 22:08:32 +0530

    Mouse actions tests

--- SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/interactions/SliderTest.java
+++ SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/interactions/SliderTest.java
@@ -1,4 +1,21 @@
 package part3_4.com.demoqa.test.part04.interactions;
 
-public class SliderTest e{
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part3_4.com.demoqa.base.BaseTest;
+
+public class SliderTest extends BaseTest {
+
+
+    @Test
+    public void testSliderResult() {
+        int x = 180;
+        int y = 0;
+       var sliderPage =  homePage.gotoWidgets().clickSlider();
+       sliderPage.moveSlider(x , y);
+       String actualValue = sliderPage.getSliderValue();
+       String expectedValue = "85";
+        Assert.assertEquals(actualValue, expectedValue, "\n Actual and Expected values do not match\n");
+
+    }
 }
