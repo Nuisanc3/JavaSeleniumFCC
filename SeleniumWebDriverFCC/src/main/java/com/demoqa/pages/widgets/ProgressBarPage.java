ec13cf4a21980e98b33853859ee1c4e3abd3a148
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-11 20:29:45 +0530

    ProgressBar Test with Fluent wait

--- SeleniumWebDriverFCC/src/main/java/com/demoqa/pages/widgets/ProgressBarPage.java
+++ SeleniumWebDriverFCC/src/main/java/com/demoqa/pages/widgets/ProgressBarPage.java
@@ -1,4 +1,24 @@
 package com.demoqa.pages.widgets;
 
+import org.openqa.selenium.By;
+
+import static utility.GetUtility.getText;
+import static utility.JavaScriptUtility.scrollToElementJS;
+import static utility.WaitUtility.fluentWaitUntilVisible;
+
 public class ProgressBarPage extends WidgetsPage {
+
+    private By startButton = By.id("startStopButton");
+    private By progressValue = By.xpath("//div[@id='progressBar']/div[@aria-valuenow='100']");
+
+
+    public String getProgressValue() {
+        fluentWaitUntilVisible(30, progressValue);
+        return getText(progressValue);
+    }
+
+    public void clickStartButton() {
+        scrollToElementJS(startButton);
+        click(startButton);
+    }
 }
--- SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/dynamic_wait/DynamicWaitTest.java
+++ SeleniumWebDriverFCC/src/test/java/part3_4/com/demoqa/test/part04/dynamic_wait/DynamicWaitTest.java
@@ -13,4 +13,15 @@ public class DynamicWaitTest extends BaseTest {
         String expectedText = "Visible After 5 Seconds";
         Assert.assertEquals(actualText, expectedText, "\n  Actual and Expected text do not match\n");
     }
+
+    @Test
+    public void testProgressBar(){
+        var progressBarPage = homePage.gotoWidgets().clickProgressBar();
+        progressBarPage.clickStartButton();
+        String actualValue = progressBarPage.getProgressValue();
+        String expectedValue = "100";
+        Assert.assertEquals(actualValue, expectedValue, "\n Value is not 100% \n ");
+
+
+    }
 }
